a970695116bfaf5951c7cc9abf065fc6db7fda05
Anish Tripathi <151549153+Anish-Tripathi@users.noreply.github.com>
2025-05-28 15:43:50 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongodb = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/DineSmart');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected");
 
         const fetchedData = await mongoose.connection.db.collection("food_items").find({}).toArray();
